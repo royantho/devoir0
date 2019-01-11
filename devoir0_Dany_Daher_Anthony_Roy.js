fa34b2ce1c8a065b74e3e94570241e4b9023c07e
DanyDaher <46608246+DanyDaher@users.noreply.github.com>
2019-01-11 16:18:32 -0500

    Add files via upload

---
+++ devoir0_Dany_Daher_Anthony_Roy.js
@@ -0,0 +1,19 @@
+//Dany Daher
+//Anthony Roy
+var prenom = "Dany";
+var nom = "Daher";
+var salutation = "Bonjour, mon nom est " + prenom + " " + nom + "!";
+var age= 20;
+var discipline = "mathématiques";
+var informations = "J'ai " + age + " ans et j'étudie en " + discipline + "!";
++ ". ";
+
+var introduction= salutation + "\n" + informations;
+
+print(introduction);
+
+load ("correction.js");
+
+
+
+
