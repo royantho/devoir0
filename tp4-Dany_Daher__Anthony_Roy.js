2be133e9746b1b89d52367822176fbf03949f995
DanyDaher <46608246+DanyDaher@users.noreply.github.com>
2019-02-09 16:39:39 -0500

    Update tp4-Dany_Daher__Anthony_Roy.js

--- tp4-Dany_Daher__Anthony_Roy.js
+++ tp4-Dany_Daher__Anthony_Roy.js
@@ -1,47 +1,45 @@
+// Équipier 1: Dany Daher
+// Équipier 2: Anthony Roy
+
 function randomBetween(a,b) {
-    var num = Math.floor(Math.pow(10,a.toString().length)*Math.random());
-        do num=Math.floor(Math.pow(10,a.toString().length)*Math.random());
-        while (num<a || num>b);
-        return num;
+    return Math.floor(Math.random() * (b - a + 1) + a);
 }
-var randomNumber = randomBetween(5,10);
-//print(randomNumber)
 
 function attaquer (viesCible, degat){
-    if (viesCible>degat) viesCible=viesCible-degat;
-    else viesCible=0;
-    return viesCible;
+    if (viesCible<degat) return 0;
+    else return viesCible-degat;
 }
-//viesCible = attaquer (60,40);
 
-var viesChevalier = randomBetween(100,200);//print(viesChevalier);
-var viesDragon = randomBetween(300,400);//print(viesDragon);
-var degatChevalier = randomBetween(50,70);//print("degatChevalier" + degatChevalier);
-var degatDragon = randomBetween(100,200);//print("degatDragon" + degatDragon);
+var viesChevalier = randomBetween(100,200);
+var viesDragon = randomBetween(300,400);
+var degatChevalier = randomBetween(50,70);
+var degatDragon = randomBetween(100,200);
 var chanceChevalier; do chanceChevalier = Math.random(); while (chanceChevalier<0.5 || chanceChevalier>0.7);
 
-//print(chanceChevalier);
-    
     print("Le combat débute!")
 
-do {
-    viesDragon=attaquer(viesDragon,degatChevalier);
+    do {
+        viesDragon=attaquer(viesDragon,degatChevalier);
+        if (viesDragon!=0){
+            print("==================================");
+            print("Vies chevalier : " + viesChevalier);
+            print("Vies Dragon : " + viesDragon);
+        
+             if (Math.random()>chanceChevalier) viesChevalier=attaquer(viesChevalier,degatDragon);
+            else print("---\nLe chevalier a évité l'attaque!");
+        }
+        else break
+    }
+    while   (viesChevalier!=0 && viesDragon!=0);
+
     print("==================================");
-    print("Vies chevalier :" + viesChevalier);
-    print("Vies Dragon :" + viesDragon);
-        if (Math.random()>chanceChevalier) viesChevalier=attaquer(viesChevalier,degatDragon);
-        else print("---\nLe chevalier a évité l'attaque!");
-}
-while   (viesChevalier!=0 && viesDragon!=0);
-
-print("==================================");
-print("Vies chevalier :" + viesChevalier);
-print("Vies Dragon :" + viesDragon);
-print("---")
+    print("Vies chevalier : " + viesChevalier);
+    print("Vies Dragon : " + viesDragon);
+    print("---")
 
 
-if (viesChevalier==0) print ("Le dragon est sorti vainqueur!! \u2694");
-else print ("Le chevalier est sorti vainqueur!! \u2694");
+if (viesChevalier==0) print ("Le chevalier a perdu!");
+else print ("Le chevalier est sorti vainqueur!!");
 
 
     
